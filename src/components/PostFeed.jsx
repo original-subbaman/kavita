3704464097ceef290a7893075dfcd03b08e9dd0a
Abhishek Subba <abhisheksubba.py@gmail.com>
2024-01-07 23:00:22 +0530

    header component and basic structure

---
+++ src/components/PostFeed.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function PostFeed(props) {
+  return (
+    <main className="flex flex-col items-center h-ful">
+      <div className="min-h font-primary text-4xl italic">
+        Once upon a time....
+      </div>
+    </main>
+  );
+}
+
+export default PostFeed;
