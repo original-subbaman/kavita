9630d63b394588ea16bbbae30f4875f642e45eec
Abhishek Subba <abhisheksubba.py@gmail.com>
2024-02-10 20:08:36 +0530

    Add Radix UI

--- src/components/Header/Profile.jsx
+++ src/components/Header/Profile.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button, Flex, Popover } from "@radix-ui/themes";
 function Profile({ name }) {
   const [anchorEl, setAnchorEl] = useState(null);
   const handleClick = (event) => {
@@ -6,13 +7,23 @@ function Profile({ name }) {
   };
   const open = Boolean(anchorEl);
   return (
-    <div className="flex items-center gap-2 rounded cursor-pointer p-2">
-      <p className="text-3xl">{name}</p>
-      <button
-        className="rounded-full border-4 border-white w-12 h-12"
-        onClick={handleClick}
-      ></button>
-    </div>
+    <Popover.Root>
+      <div className="flex items-center gap-2 rounded cursor-pointer p-2">
+        <p className="text-3xl">{name}</p>
+        <Popover.Trigger>
+          <button
+            className="rounded-full border-4 border-white w-12 h-12"
+            onClick={handleClick}
+          ></button>
+        </Popover.Trigger>
+      </div>
+      <Popover.Content>
+        <Flex direction="column" gap="2">
+          <Button variant="ghost">Profile</Button>
+          <Button variant="ghost">Log out</Button>
+        </Flex>
+      </Popover.Content>
+    </Popover.Root>
   );
 }
 
---
+++ src/components/Header/ProfilePopover.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Popover } from "@radix-ui/themes";
+function ProfilePopOver(props) {
+  return <Popover.Root></Popover.Root>;
+}
+
+export default ProfilePopOver;
