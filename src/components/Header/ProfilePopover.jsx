b2d6d6e8499e8032aec55d44d645bf13c0666855
Abhishek Subba <abhisheksubba.py@gmail.com>
2024-02-12 14:33:17 +0530

    Add profile pop over menu

--- src/components/Header/ProfilePopover.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Popover } from "@radix-ui/themes";
-function ProfilePopOver(props) {
-  return <Popover.Root></Popover.Root>;
-}
-
-export default ProfilePopOver;
