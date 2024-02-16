a12591e2c47fbd6bc2000c3496943517db936168
Abhishek Subba <abhisheksubba.py@gmail.com>
2024-02-16 18:27:18 +0530

    Update header to sticky header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import Profile from "./Profile";
 import { Text } from "@radix-ui/themes";
 function Header(props) {
   return (
-    <header className="flex justify-between text-white bg-dark items-center h-16 px-8 drop-shadow-md">
+    <header className="flex justify-between text-white bg-dark items-center h-16 px-8 drop-shadow-md sticky top-0 z-[100]">
       <Text size="6">CWS</Text>
       <Profile name="John Doe" />
     </header>
