7a5bc05813801a98dad1011781f494a969db401c
Abhishek Subba <abhisheksubba.py@gmail.com>
2025-03-25 21:42:11 +0530

    feat(header): Update active header to be bold and animate underline

--- src/components/Header/LinkText.jsx
+++ src/components/Header/LinkText.jsx
@@ -2,13 +2,18 @@ import { Text } from "@radix-ui/themes";
 
 const LinkText = ({ isActive, children }) => {
   return (
-    <Text
-      className={`hover:text-green-500 duration-300 transition-all ${
-        isActive ? "text-green-500" : "text-[#30a46c]"
-      }`}
-    >
-      {children}
-    </Text>
+    <div>
+      <Text
+        className={`hover:text-green-500 duration-300 transition-all ${
+          isActive ? "text-green-500 font-bold" : "text-[#30a46c]"
+        }`}
+      >
+        {children}
+      </Text>
+      {isActive && (
+        <div className="animate-width-grow h-1 bg-radix-grass rounded-sm"></div>
+      )}
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      keyframes: {
+        widthGrow: {
+          '0%': { width: "0%"},
+          '100%': { width: '40%'}
+        }
+      },
+      animation: {
+        'width-grow': 'widthGrow 0.3s ease-out forwards' 
+      },
       colors: {
       "dark": "#222222", 
       "dark-light": "#191919",
