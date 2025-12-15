055f328bbfe788f4c08118f28839873b9d906f9e
original-subbaman <abhisheksubba.py@gmail.com>
2025-12-15 21:57:11 +0530

    feat(ResponseSnackbar): update bg color for all severity types with radix theme colors

--- src/components/ResponseSnackbar.jsx
+++ src/components/ResponseSnackbar.jsx
@@ -9,6 +9,24 @@ const ResponseSnackbar = ({
 }) => {
   return (
     <Snackbar
+      sx={{
+        "& .MuiAlert-filledSuccess": {
+          backgroundColor: "#30a46c", // radix-green
+          color: "#fff",
+        },
+        "& .MuiAlert-filledInfo": {
+          backgroundColor: "#0091ff", // radix-blue-9
+          color: "#fff",
+        },
+        "& .MuiAlert-filledWarning": {
+          backgroundColor: "#f5d90a", // radix-yellow-9
+          color: "#222",
+        },
+        "& .MuiAlert-filledError": {
+          backgroundColor: "#e5484d", // radix-red-9
+          color: "#fff",
+        },
+      }}
       anchorOrigin={position}
       open={open}
       autoHideDuration={autoHideDuration}
