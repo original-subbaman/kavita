8945be64cfcdcd0cf8b4d5820f93af9507330acc
original-subbaman <abhisheksubba.py@gmail.com>
2026-02-28 16:52:56 +0530

    feat: update the UI of 404 page to match the new design language of the app

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,32 +1,23 @@
 import { useNavigate } from "react-router-dom";
-import RootWrapper from "../components/RootWrapper";
-const NavigateButton = ({ title, onClick }) => {
-  return (
-    <button
-      onClick={onClick}
-      className="bg-slate-300 hover:bg-slate-400 transition-colors duration-300 p-1 px-6 rounded-lg text-dark font-bold"
-    >
-      {title}
-    </button>
-  );
-};
+import { Button } from "../components/ui/Button";
+
 const PageNotFound = () => {
   const navigate = useNavigate();
   const navigateToHome = () => navigate("/");
   const navigateToLogin = () => navigate("/login");
   const navigateToRegister = () => navigate("/signup");
   return (
-    <div className="w-full font-primary min-h-screen bg-dark-light">
+    <div className="w-full font-primary min-h-screen">
       <div className="w-full h-[100vh] font-mono flex flex-col text-white items-center justify-center">
-        <p className="text-8xl font-mono text-gray-400">404</p>
-        <p className="text-2xl font-mono font-semibold mb-4">
+        <p className="text-8xl text-gray-400">404</p>
+        <p className="text-2xl text-foreground  mb-4">
           Oops! We couldn't find that page.
         </p>
         <p className="text-xl font-light">Maybe try the following page:</p>
         <div className="flex gap-4 mt-4">
-          <NavigateButton title={"Home"} onClick={navigateToHome} />
-          <NavigateButton title={"Login"} onClick={navigateToLogin} />
-          <NavigateButton title={"Register"} onClick={navigateToRegister} />
+          <Button onClick={navigateToHome}>Home</Button>
+          <Button onClick={navigateToLogin}>Login</Button>
+          <Button onClick={navigateToRegister}>Register</Button>
         </div>
       </div>
     </div>
